3cdf308873c5bc0b83f9b77e10de1f9c8eb323b0
FR33THY <fr33thy@gmail.com>
2026-04-05 01:36:04 +1100

    No Antivirus False Positive
    Fix uninstall brlapi for multi lang

--- Temp/steptwo.ps1
+++ Temp/steptwo.ps1
@@ -790,7 +790,7 @@ Disable-WindowsOptionalFeature -Online -FeatureName $_.FeatureName -NoRestart -W
 cmd /c "sc stop `"brlapi`" >nul 2>&1"
 cmd /c "sc delete `"brlapi`" >nul 2>&1"
 cmd /c "takeown /f `"$env:SystemRoot\brltty`" /r /d y >nul 2>&1"
-cmd /c "icacls `"$env:SystemRoot\brltty`" /grant administrators:F /t >nul 2>&1"
+cmd /c "icacls `"$env:SystemRoot\brltty`" /grant *S-1-5-32-544:F /t >nul 2>&1"
 Remove-Item "$env:SystemRoot\brltty" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 
 # uninstall microsoft gameinput
